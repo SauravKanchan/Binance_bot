fb3df3c478cb3643ed1303815ddb29e9a289f40c
SauravKanchan <sauravnk30@gmail.com>
2018-04-14 23:44:34 +0530

    Remove buy/sell question

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ def process_message(msg):
     # print("message type: {}".format(msg['e']))
     global last_order
     print(msg['p'])
-    if msg['p'] <= buy_price and last_order == 'sell':
-        print('Buying {1} coins at {0} price'.format(msg['p'],quantity))
-        order = client.order_limit_buy(symbol=coin,quantity=quantity,price=msg['p'])
-        last_order = 'buy'
-    elif msg['p'] >= sell_price and last_order == 'buy':
-        print('Selling {1} coins at {0} price'.format(msg['p'],quantity))
-        order = client.order_limit_sell(symbol=coin,quantity=quantity,price=msg['p'])
-        last_order = 'sell'
+    if msg['p'] <= buy_price :
+        try:
+            order = client.order_limit_buy(symbol=coin,quantity=quantity,price=msg['p'])
+            print('Bought {1} coins at {0} price'.format(msg['p'],quantity))
+            last_order = 'buy'
+        except BinanceAPIException as e:
+            print(e.message)
+    elif msg['p'] >= sell_price:
+        try:
+            order = client.order_limit_sell(symbol=coin,quantity=quantity,price=msg['p'])
+            print('Sold {1} coins at {0} price'.format(msg['p'],quantity))
+            last_order = 'sell'
+        except BinanceAPIException as e:
+            print(e.message)
 def average_price(prices):
     total = 0
     for i in prices:total+=float(i['p'])
@@ -32,8 +38,6 @@ coin = input('Enter coin to trade(Ex:- BNBBTC ): ')
 prices = client.get_aggregate_trades(symbol=coin)
 print("Current average price of",coin,"is",average_price(prices))
 print("Last price of",coin,"is",prices[-1]['p'])
-
-last_order = input('What was your last order buy/sell: ')
 buy_price = input('Enter your buying price: ')
 sell_price = input('Enter your selling price: ')
 quantity = input('Enter quantity: ')
