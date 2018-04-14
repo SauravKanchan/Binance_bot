6357f6c0d420f210c0bc3c75acca1438978a8641
SauravKanchan <sauravnk30@gmail.com>
2018-04-15 03:44:53 +0530

    Trade with fractions instead of quantity

--- main.py
+++ main.py
@@ -11,31 +11,39 @@ from binance.exceptions import BinanceAPIException, BinanceWithdrawException
 import os
 
 orders = []
+last_error_message=''
+quantity = 0
 def process_message(msg):
     # print("message type: {}".format(msg['e']))
-    # global orders,buy_price,sell_price
+    global quantity,last_error_message
     os.system('clear')
     print('Trading:',coin)
-    print('Quantity:',quantity)
+    print('Fraction:',fraction)
     print('Buying price:',buy_price)
     print('Selling price:',sell_price)
+    print('Profit percentage:',profit)
     print('Current price: ',msg['p'])
+    print('Quantity',quantity)
+    print('last_error_message:',last_error_message)
+    print('\nOrders placed:-')
     for i in orders:print(i)
 
     if msg['p'] <= buy_price :
         try:
+            quantity = float(client.get_asset_balance(asset=sell)['free'])*fraction/float(msg['p'])
             order = client.order_limit_buy(symbol=coin,quantity=quantity,price=msg['p'])
             orders.append('Bought {1} coins at {0} price'.format(msg['p'],quantity))
-        except:
+        except Exception  as e:
             # print(e.message)
-            pass
+            last_error_message = e
     elif msg['p'] >= sell_price:
         try:
+            quantity = float(client.get_asset_balance(asset=buy)['free'])*fraction
             order = client.order_limit_sell(symbol=coin,quantity=quantity,price=msg['p'])
             orders.append('Sold {1} coins at {0} price'.format(msg['p'],quantity))
-        except:
+        except Exception  as e:
             # print(e.message)
-            pass
+            last_error_message = e
 def average_price(prices):
     total = 0
     for i in prices:total+=float(i['p'])
@@ -43,13 +51,23 @@ def average_price(prices):
 
 client = Client(api_key, api_secret)
 
-coin = input('Enter coin to trade(Ex:- BNBBTC ): ')
-prices = client.get_aggregate_trades(symbol=coin)
+coin1,coin2 = input('Enter coins to trade(Ex:- BNB USDT ): ').upper().split()
+try:
+    coin = coin1+coin2
+    prices = client.get_aggregate_trades(symbol=coin)
+    buy = coin1
+    sell = coin2
+except:
+    coin = coin2+coin1
+    prices = client.get_aggregate_trades(symbol=coin)
+    buy = coin2
+    sell = coin1
 print("Current average price of",coin,"is",average_price(prices))
 print("Last price of",coin,"is",prices[-1]['p'])
 buy_price = input('Enter your buying price: ')
 sell_price = input('Enter your selling price: ')
-quantity = input('Enter quantity: ')
+fraction = input('Enter fraction of quantity(ex: 0.5): ')
+fraction = float(fraction)
 
 profit=(((float(sell_price)-float(buy_price))/float(buy_price))*100)
 if profit<=0.2:
