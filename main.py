338f6ac1c48e539db15748375fc584daafb82f38
SauravKanchan <sauravnk30@gmail.com>
2018-04-14 21:13:28 +0530

    Update main

--- main.py
+++ main.py
@@ -8,25 +8,36 @@ except:
 from binance.websockets import BinanceSocketManager
 from binance.enums import *
 
-coin = raw_input('Enter coin to trade(Ex:- BNBBTC ): ')
-last_order = raw_input('What was your last order buy/sell: ')
-buy_price = raw_input('Enter your buying price: ')
-sell_price = raw_input('Enter your selling price: ')
-quantity = raw_input('Enter quantity: ')
 
 def process_message(msg):
     # print("message type: {}".format(msg['e']))
     global last_order
     print(msg['p'])
     if msg['p'] <= buy_price and last_order == 'sell':
-        print('Buying {0} coins at {1} price'.format(msg['p'],quantity))
-        order = client.order_limit_buy(symbol=coin,quantity=quantity,price=buy_price)
+        print('Buying {1} coins at {0} price'.format(msg['p'],quantity))
+        order = client.order_limit_buy(symbol=coin,quantity=quantity,price=msg['p'])
         last_order = 'buy'
     elif msg['p'] >= sell_price and last_order == 'buy':
-        print('Selling {0} coins at {1} price'.format(msg['p'],quantity))
-        order = client.order_limit_sell(symbol=coin,quantity=quantity,price=sell_price)
+        print('Selling {1} coins at {0} price'.format(msg['p'],quantity))
+        order = client.order_limit_sell(symbol=coin,quantity=quantity,price=msg['p'])
         last_order = 'sell'
+def average_price(prices):
+    total = 0
+    for i in prices:total+=float(i['p'])
+    return total/len(prices)
+
 client = Client(api_key, api_secret)
+
+coin = input('Enter coin to trade(Ex:- BNBBTC ): ')
+prices = client.get_aggregate_trades(symbol=coin)
+print("Current average price of",coin,"is",average_price(prices))
+print("Last price of",coin,"is",prices[-1]['p'])
+
+last_order = input('What was your last order buy/sell: ')
+buy_price = input('Enter your buying price: ')
+sell_price = input('Enter your selling price: ')
+quantity = input('Enter quantity: ')
+print("Profit margin: ",(((int(sell_price)-int(buy_price))/int(buy_price))*100),"%")
 
 bm = BinanceSocketManager(client)
 bm.start_aggtrade_socket(coin, process_message)
